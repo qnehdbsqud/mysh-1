f84b08e5cbe23e2940cf88b823c78db165096632
kimbyungnam <kimbyungnam@ajou.ac.kr>
2017-11-13 08:42:09 -0800

    pipe success not multi piped

--- src/commands.c
+++ src/commands.c
@@ -5,11 +5,18 @@
 #include <assert.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
 
 #include "commands.h"
 #include "built_in.h"
 
+#define FILE_SERVER "/tmp/test_server"
+
 pid_t proc_create(struct single_command,int index_of_commands,int* result);
+void* server();
 
 static struct built_in_command built_in_commands[] = {
   { "cd", do_cd, validate_cd_argv },
@@ -36,10 +43,23 @@ static int is_built_in_command(const char* command_name)
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
 int result=0;
+pid_t pid2;
+int status;
+int temp_in, temp_out;
+pthread_t server_thread;
+
+temp_in = dup(0);
+
+int client_sock;
+struct sockaddr_un server_addr;
+
   if (n_commands > 0) {
     struct single_command* com = (*commands);
 
     assert(com->argc != 0);
+if(n_commands>1){
+	pthread_create(&server_thread,NULL,server,NULL);
+}
 for(int i = 0;i<n_commands;i++){
     int built_in_pos = is_built_in_command(com[i].argv[0]);
     if (built_in_pos != -1) {
@@ -56,13 +76,87 @@ for(int i = 0;i<n_commands;i++){
     } else if (strcmp(com[i].argv[0], "exit") == 0) {
       return 1;
     } else {
-	proc_create(com[i],i,&result);
-	if(result==-1)
-		return -1;
-    }
+	if(n_commands>1&&i==0){
+		pid2=fork();
+		if(!pid2){//child
+			client_sock = socket(PF_FILE, SOCK_STREAM,0);
+			if(client_sock == -1){
+				fprintf(stderr,"client socket error\n");
+				return -1;
+			}
+			memset(&server_addr,0,sizeof(server_addr));
+			server_addr.sun_family = AF_UNIX;
+			strcpy(server_addr.sun_path, FILE_SERVER);
+	
+			while(-1==connect(client_sock, (struct sockaddr*)&server_addr,sizeof(server_addr))){
+				fprintf(stderr,"client connect error\n");
+				sleep(2);
+			}
+			fprintf(stderr,"client connect success\n");		
+			temp_out = dup(1);
+			dup2(client_sock,1);
+
+			proc_create(com[i],i,&result);
+			
+			dup2(temp_out,1);
+			exit(1);
+		}
+		else{//parent
+			waitpid(pid2,&status,0);
+		}
+	}
+	else{//one command
+		proc_create(com[i],i,&result);
+		if(i>0){
+			dup2(temp_in,0);
+		}
+		if(result==-1)
+			return -1;
+	}
+
+
+    }//else
   }//for
+	if(n_commands>1){
+		close(client_sock);
+		pthread_join(server_thread,(void**)&status);
+	}
 }//if
   return 0;
+}
+
+void* server()
+{	
+	int server_sock, client_sock;
+	
+	if(0==access(FILE_SERVER,F_OK))
+		unlink(FILE_SERVER);
+
+	server_sock = socket(PF_FILE, SOCK_STREAM, 0);
+	struct sockaddr_un server_addr, client_addr;
+	int client_addr_size=sizeof(client_addr);
+	memset(&server_addr,0,sizeof(server_addr));
+	server_addr.sun_family = AF_UNIX;
+	strcpy(server_addr.sun_path, FILE_SERVER);
+
+	if(-1== bind(server_sock,(struct sockaddr*)&server_addr,sizeof(server_addr))){
+		fprintf(stderr,"server bind error\n");
+	}
+	
+	while(-1==listen(server_sock,10)){
+		fprintf(stderr,"server listen error\n");
+	}
+
+	client_sock = accept(server_sock,(struct sockaddr*)&client_addr,&client_addr_size);
+	if(client_sock==-1){
+		fprintf(stderr,"client accept error\n");
+	}
+	
+	dup2(client_sock,0);
+	
+	close(server_sock);
+	close(client_sock);
+	pthread_exit(0);
 }
 
 void free_commands(int n_commands, struct single_command (*commands)[512])
